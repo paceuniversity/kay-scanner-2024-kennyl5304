e5e81dd7fc9a1e0f3adbaac7bb5577ac04e9dd9f
Kenny <55723129+kennyl5304@users.noreply.github.com>
2024-11-18 14:31:02 -0500

    Update TokenStream.java

--- src/main/java/com/scanner/project/TokenStream.java
+++ src/main/java/com/scanner/project/TokenStream.java
@@ -90,7 +90,7 @@ public class TokenStream {
 					t.setValue(t.getValue() + nextChar);
 					nextChar = readChar();
 				} else {
-					t.setType("Other");
+					t.setValue(t.getValue());
 				}
 				return t;
 			case '>':
@@ -100,7 +100,7 @@ public class TokenStream {
 					t.setValue(t.getValue() + nextChar);
 					nextChar = readChar();
 				} else {
-					t.setType("Other");
+					t.setValue(t.getValue());
 				}
 				return t;
 			case '=':
@@ -280,7 +280,7 @@ public class TokenStream {
 			case '{':
 			case '}':
 			case ';':
-			case '.':
+			case ',':
 				return true;
 			default:
 				return false;
@@ -296,6 +296,7 @@ public class TokenStream {
 			case '*':
 			case '/':
 			case '<':
+			case '>':
 			case '=':
 			case '!':
 			case '&':
